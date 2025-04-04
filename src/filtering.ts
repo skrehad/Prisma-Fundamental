ae3b04a187da7208d329ca3154368aea72c0bd40
rehadhasan <rehadhasan664@gmail.com>
2025-04-05 02:42:29 +0600

    Filtering data in prisma

---
+++ src/filtering.ts
@@ -0,0 +1,93 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+const filtering = async () => {
+  const andFiltering = await prisma.post.findMany({
+    where: {
+      AND: [
+        {
+          title: {
+            contains: "title",
+          },
+        },
+        {
+          published: true,
+        },
+      ],
+    },
+  });
+
+  //   console.log(andFiltering);
+
+  const orFiltering = await prisma.post.findMany({
+    where: {
+      OR: [
+        {
+          title: {
+            contains: "title",
+          },
+        },
+        {
+          published: true,
+        },
+      ],
+    },
+  });
+
+  //   console.log(orFiltering);
+
+  const notFiltering = await prisma.post.findMany({
+    where: {
+      NOT: [
+        {
+          title: {
+            contains: "this",
+          },
+        },
+      ],
+    },
+  });
+
+  //   console.log(notFiltering);
+
+  const startsWith = await prisma.user.findMany({
+    where: {
+      email: {
+        startsWith: "user1@gmail.com", // endsWith, contains, equals
+      },
+    },
+  });
+  //   console.log(startsWith);
+
+  //   const userNameArray = ["user1", "user2", "user5"];
+
+  //   const userNamesByArray = await prisma.user.findMany({
+  //     where: {
+  //       username: {
+  //         in: userNameArray,
+  //       },
+  //     },
+  //   });
+
+  //   const inDepthData = await prisma.user.findUnique({
+  //     where: {
+  //       id: 3,
+  //     },
+  //     include: {
+  //       post: {
+  //         include: {
+  //           postCategory: {
+  //             include: {
+  //               category: true,
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   });
+
+  //   console.dir(inDepthData, { depth: Infinity });
+};
+
+filtering();
