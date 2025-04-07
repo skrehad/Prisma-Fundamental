85e91cc8389b495f7fb133f951bdc1ec6f9a0912
rehadhasan <rehadhasan664@gmail.com>
2025-04-07 21:48:21 +0600

    explore group by prisma

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -3,12 +3,48 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 const aggregate = async () => {
+  // find average age
   const avgAge = await prisma.user.aggregate({
     _avg: {
       age: true,
     },
   });
-  console.log(avgAge);
+
+  // find sum of age
+  const sumAge = await prisma.user.aggregate({
+    _sum: {
+      age: true,
+    },
+  });
+
+  // find count of age fields
+  const countAge = await prisma.post.aggregate({
+    _count: {
+      title: true,
+    },
+    where: {
+      published: true,
+    },
+  });
+
+  // find number of records
+  const countData = await prisma.user.count();
+
+  // find max age
+  const maxAge = await prisma.user.aggregate({
+    _max: {
+      age: true,
+    },
+  });
+
+  // find min age
+  const minAge = await prisma.user.aggregate({
+    _min: {
+      age: true,
+    },
+  });
+
+  console.log(minAge);
 };
 
 aggregate();
