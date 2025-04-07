3529621f305d442e249a8fc27dd9d15be67cdf8e
rehadhasan <rehadhasan664@gmail.com>
2025-04-07 22:55:14 +0600

    delete all data

--- src/raw-query.ts
+++ src/raw-query.ts
@@ -3,11 +3,11 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 const rawQuery = async () => {
-  //const posts = await prisma.$queryRaw`SELECT * FROM "posts"`
-  //console.log(posts)
+  const posts = await prisma.$queryRaw`SELECT * FROM "posts"`;
+  console.log(posts);
 
   // delete users table data
-  await prisma.$queryRaw`TRUNCATE TABLE "categories" CASCADE`;
+  await prisma.$queryRaw`TRUNCATE TABLE "users" CASCADE`;
 };
 
 rawQuery();
